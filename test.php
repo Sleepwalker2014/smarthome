06dca828013de77c285cb459b0dfef6b7857a61a
Marcel Roa <marcel@roa-pc.marcel.roa@limetec-biotechnologies.de>
2020-04-03 13:17:02 +0200

    change led value

--- test.php
+++ test.php
@@ -8,4 +8,4 @@ const DIGITAL_FALSE = 0;
 echo $_POST['value'];
 
 shell_exec('gpio -g mode 25 '.GPIO_MODES['output']);
-shell_exec('gpio -g write 25 '.($_POST['value'] ? DIGITAL_TRUE : DIGITAL_FALSE));
+shell_exec('gpio -g write 25 '.(filter_var($_POST['value'], FILTER_VALIDATE_BOOLEAN) ? DIGITAL_TRUE : DIGITAL_FALSE));
