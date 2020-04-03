55c01b4173e5cfd98ec6a77c91b729e507dc9f5e
Marcel Roa <marcel@roa-pc.marcel.roa@limetec-biotechnologies.de>
2020-04-03 12:54:19 +0200

    send command

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
 
-echo 'blubberman';
-shell_exec('gpio -g mode 25 out');
-shell_exec('gpio -g write 25 1');
+const GPIO_MODES = ['input' => 'in', 'output' => 'out'];
+
+const DIGITAL_TRUE = 1;
+const DIGITAL_FALSE = 0;
+
+echo $_POST['value'];
+
+shell_exec('gpio -g mode 25 '.GPIO_MODES['output']);
+shell_exec('gpio -g write 25 '.($_POST['value'] ? DIGITAL_TRUE : DIGITAL_FALSE));
